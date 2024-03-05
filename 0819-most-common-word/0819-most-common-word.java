39024eead7b0b05fe43a64dc01fda46247c2f7f1
ykmxxi <ykmxxi97@gmail.com>
2024-03-05 15:05:18 +0900

    Time: 11 ms (86.84%), Space: 43.1 MB (41.57%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.java
@@ -0,0 +1,25 @@
+/*
+    - 금지된 단어를 제외하고 가장 흔하게 등장하는 단어를 출력
+    - 대소문자 구분 X, 마침표 쉼표 등은 무시
+*/
+
+import java.util.*;
+
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        String[] words = paragraph.replaceAll("\\W+", " ").toLowerCase().split(" ");
+        
+        Set<String> bans = new HashSet<>(Arrays.asList(banned));
+        Map<String, Integer> cnts = new HashMap<>();
+        
+        for (String word : words) {
+            if (bans.contains(word)) {
+                continue;
+            }
+            cnts.put(word, cnts.getOrDefault(word, 0) + 1);
+        }
+        
+        
+        return Collections.max(cnts.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+}
