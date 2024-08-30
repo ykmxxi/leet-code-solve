25f47959866860d09015c290305fb72b8418c968
ykmxxi <ykmxxi97@gmail.com>
2024-08-30 10:21:51 +0900

    [Medium] Time: 0 ms (100.00%), Memory: 41.1 MB (52.62%) - LeetHub

--- LeetCode/Medium/0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ LeetCode/Medium/0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -6,25 +6,17 @@
 
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        ListNode node = new ListNode(0);
-        ListNode temp = node;
-        node.next = head;
-
-        // 시작 노드 0: 연결 리스트의 head를 next로 갖는 임시 노드
-        while (node.next != null && node.next.next != null) {
-            // node: 현재 노드, a: 현재 노드의 다음 노드, b: 현재 노드의 다음 다음 노드
-            ListNode a = node.next;
-            ListNode b = node.next.next;
-
-            // swap후 다음 노드를 설정
-            a.next = b.next;
-            // swap
-            node.next = b;
-            node.next.next = a;
-
-            // swap 후 2칸 이동
-            node = node.next.next;
+        // 현재 노드와 다음 노드가 있으면 반복
+        if (head != null && head.next != null) {
+            // 다음 노드 선언
+            ListNode p = head.next;
+            // 다음 다음 노드를 파라미터로 전달하고 스왑된 값을 리턴 받음
+            head.next = swapPairs(head.next.next);
+            // 다음 다음 노드는 현재 노드로 지정
+            p.next = head;
+            // 다음 노드 리턴
+            return p;
         }
-        return temp.next;
+        return head;
     }
 }
