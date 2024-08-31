3b7f82303454e984f02e02481f55f6a8edef8826
ykmxxi <ykmxxi97@gmail.com>
2024-08-31 10:24:51 +0900

    [Easy] Time: 2 ms (52.27%), Memory: 41.7 MB (24.73%) - LeetHub

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -7,27 +7,27 @@ import java.util.*;
 class Solution {
 
     public boolean isValid(String s) {
-        Deque<Character> dq = new ArrayDeque<>();
+        // 유효성 검증을 위한 스택 선언
+        Deque<Character> stack = new ArrayDeque<>();
+        // 유효성 검증을 위한 매핑 테이블
+        Map<Character, Character> table = new HashMap<>() {{
+            put(')', '(');
+            put('}', '{');
+            put(']', '[');
+        }};
 
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[') {
-                dq.push(ch);
-            } else {
-                if (dq.isEmpty()) {
-                    return false;
-                }
-                if (ch == ')' && dq.peek() == '(') {
-                    dq.pop();
-                } else if (ch == '}' && dq.peek() == '{') {
-                    dq.pop();
-                } else if (ch == ']' && dq.peek() == '[') {
-                    dq.pop();
-                } else {
-                    return false;
-                }
+        // 문자열을 문자 단위로 반복
+        for (int i = 0; i < s.length(); i++) {
+            // 닫힘 괄호가 아닌 경우 스택에 푸시
+            if (!table.containsKey(s.charAt(i))) {
+                stack.push(s.charAt(i));
+                // 중간에 스택이 비거나 팝 결과가 열림 괄호가 아닌 경우 유효하지 않음
+            } else if (stack.isEmpty() || table.get(s.charAt(i)) != stack.pop()) {
+                return false;
             }
         }
-        return dq.isEmpty();
+        // 유효한 입력이 되려면 반복 완료 후 스택이 비어야 한다.
+        return stack.isEmpty();
     }
 
 }
