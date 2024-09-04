91a8f66e3f8503bdceb191e5f75d0cb7eec75328
ykmxxi <ykmxxi97@gmail.com>
2024-09-04 14:57:30 +0900

    [Hard] Time: 5 ms (40.33%), Memory: 44.2 MB (70.47%) - LeetHub

--- LeetCode/Hard/0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ LeetCode/Hard/0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -7,26 +7,38 @@ import java.util.*;
 
 class Solution {
 
-    public static ListNode mergeKLists(ListNode[] lists) {
-        Queue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((o1, o2) -> {
+            // 동일한 값이면 무시
+            if (o1.val == o2.val)
+                return 0;
+                // 새로운 값(o1)이 기본 값(o2)보다 크면 뒤에 위치
+            else if (o1.val > o2.val)
+                return 1;
+                // 작으면 앞에 위치
+            else
+                return -1;
+        });
 
-        for (ListNode list : lists) {
-            ListNode cur = list;
-            while (cur != null) {
-                pq.offer(cur);
-                cur = cur.next;
-            }
-        }
+        ListNode root = new ListNode(0);
+        ListNode tail = root;
 
-        ListNode head = new ListNode(0);
-        ListNode node = head;
+        // 각 연결 리스트의 첫 번째 노드를 큐에 저장
+        for (ListNode node : lists)
+            if (node != null)
+                pq.add(node);
+
+        // 큐가 모두 빌때까지 반복
         while (!pq.isEmpty()) {
-            node.next = pq.poll();
-            node = node.next;
-        }
-        node.next = null; // 마지막 노드가 갖고 있는 기존 next 참조 끊기
+            // 우선순위에 따라 추출, 다음 노드로 이동
+            tail.next = pq.poll();
+            tail = tail.next;
 
-        return head.next;
+            // 추출한 연결 리스트의 다음 노드는 다시 큐에 저장
+            if (tail.next != null)
+                pq.add(tail.next);
+        }
+        return root.next;
     }
 
 }
