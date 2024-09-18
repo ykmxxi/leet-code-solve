f3b70a58c98528c92234f9487e10b1f6a6efd3eb
ykmxxi <ykmxxi97@gmail.com>
2024-09-18 11:58:49 +0900

    [Easy] Time: 0 ms (100.00%), Memory: 44.8 MB (67.70%) - LeetHub

---
+++ LeetCode/Easy/0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -0,0 +1,19 @@
+/*
+    - 두 이진 트리를 병합, 노드가 겹치는 경우 값을 합해 노드의 새 값으로
+*/
+class Solution {
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
+
+}
