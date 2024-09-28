085e5c1231a235ce823aead3425eaa4e2b36436e
ykmxxi <ykmxxi97@gmail.com>
2024-09-28 12:12:10 +0900

    [Medium] Time: 0 ms (100.00%), Memory: 41.2 MB (31.68%) - LeetHub

--- LeetCode/Medium/1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
+++ LeetCode/Medium/1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -1,49 +1,25 @@
-
 /*
     - BST의 루트가 주어질 때, 모든 노드의 값을 (원래 값 + 자신 보다 큰 노드 값들의 합)로 변경
     - root 무조건 존재
 */
 
-import java.util.*;
-
 class Solution {
 
-    Deque<Integer> dq = new ArrayDeque<>();
-    Deque<Integer> prefix = new LinkedList<>();
+    int sum = 0;
 
     public TreeNode bstToGst(TreeNode root) {
-        // BST 중위 순회 -> 오름차순 정렬 순서대로 방문
         dfs(root);
-        // 중위 순회 결과 계산
-        int sum = 0;
-        while (!dq.isEmpty()) {
-            int cur = dq.poll();
-            sum += cur;
-            prefix.push(sum); // 누적합을 스택에 저장
-        }
-
-        // 다시 중위 순회를 돌며 값을 변경
-        change(root);
         return root;
     }
 
     public void dfs(TreeNode root) {
-        if (root.left != null) {
-            dfs(root.left);
-        }
-        dq.push(root.val);
         if (root.right != null) {
             dfs(root.right);
         }
-    }
-
-    public void change(TreeNode root) {
+        sum += root.val;
+        root.val = sum;
         if (root.left != null) {
-            change(root.left);
-        }
-        root.val = prefix.poll();
-        if (root.right != null) {
-            change(root.right);
+            dfs(root.left);
         }
     }
 
